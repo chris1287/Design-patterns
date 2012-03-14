a8b6236433bcc69731579ab7f0df876f2594a905
Cristiano Prato <mmes@tagore.zirak.it>
2012-03-14 15:32:43 +0100

    Add Chain of Responsibility pattern.

---
+++ ChainOfRequest/Handler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChainOfRequest
+{
+    abstract class Handler
+    {
+        protected Handler _successor;
+
+        public void SetSuccessor(Handler successor)
+        {
+            _successor = successor;
+        }
+
+        public abstract void HandleRequest(int request);
+    }
+
+    class HandleOdd : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request % 2 != 0)
+            {
+                Console.WriteLine("{0} handles {1}", GetType().Name, request);
+            }
+            else if (_successor != null && _successor != this)
+            {
+                _successor.HandleRequest(request);
+            }
+            else
+            {
+                Console.WriteLine("{0} will not be handled");
+            }
+        }
+    }
+
+    class HandleEven : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request % 2 == 0)
+            {
+                Console.WriteLine("{0} handles {1}", GetType().Name, request);
+            }
+            else if (_successor != null && _successor != this)
+            {
+                _successor.HandleRequest(request);
+            }
+            else
+            {
+                Console.WriteLine("{0} will not be handled");
+            }
+        }
+    }
+}
---
+++ ChainOfRequest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChainOfRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] request = { 2, 4, 1, 3, 7, 8 };
+
+            Handler he = new HandleEven();
+            Handler ho = new HandleOdd();
+
+            he.SetSuccessor(ho);
+
+            foreach (int r in request)
+            {
+                he.HandleRequest(r);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
